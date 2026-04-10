63c10511cb0d6af61a4465feb098a7cb1bcc06aa
IFTICENE <chermatti.31@gmail.com>
2026-04-09 22:39:31 -0400

    Étape 2 - ajout voiture

--- crud_db.py
+++ crud_db.py
@@ -1,5 +1,9 @@
 import json
 import mysql.connector
+from voiture import Voiture
+
+
+#Methode1 connecter_db
 
 def connecter_db():
     with open("config.json", "r") as fichier:
@@ -13,3 +17,32 @@ def connecter_db():
     )
 
     return connexion
+
+#methode2 ajouter_voiture
+
+def ajouter_voiture(voiture):
+    connexion = connecter_db()
+    curseur = connexion.cursor()
+
+    curseur.execute("""
+        CREATE TABLE IF NOT EXISTS voiture (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            marque VARCHAR(100),
+            modele VARCHAR(100),
+            annee INT,
+            prix DECIMAL(12,2)
+        )
+    """)
+
+    requete = """
+        INSERT INTO voiture (marque, modele, annee, prix)
+        VALUES (%s, %s, %s, %s)
+    """
+
+    valeurs = (voiture.marque, voiture.modele, voiture.annee, voiture.prix)
+
+    curseur.execute(requete, valeurs)
+    connexion.commit()
+
+    curseur.close()
+    connexion.close()
--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from crud_db import connecter_db
+import mysql.connector
+from voiture import Voiture
+from crud_db import ajouter_voiture, connecter_db
 
-conn = connecter_db()
-print("Connexion OK")
-conn.close()
+connexion = None
 
+try:
+    connexion = connecter_db()
+    print("Connexion réussie !")
+except Exception as e:
+    print("Erreur de connexion :", e)
+finally:
+    if connexion is not None:
+        connexion.close()
 
+v1 = Voiture("AUDI", "A3", 2025, 60000)
+v2 = Voiture("BMW", "M3", 2026, 65000)
 
+ajouter_voiture(v1)
+ajouter_voiture(v2)
+
+print("Voiture ajoutée")
