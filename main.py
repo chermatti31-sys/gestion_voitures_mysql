c1f4e0a3acf675da49ae1c807eaa9f102fdffc0f
IFTICENE <chermatti.31@gmail.com>
2026-04-09 23:37:55 -0400

    Affichage voitures après suppression Mercedes

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from voiture import Voiture
 from crud_db import ajouter_voiture, supprimer_voiture, recuperer_voitures
 
 
-supprimer_voiture(1)
+voitures = recuperer_voitures()
 
-print("Mercedes supprimée avec succès")
+print("Liste des voitures dans la base :")
+
+for v in voitures:
+    v.afficher_voiture()
