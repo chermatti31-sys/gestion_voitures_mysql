f74b8efd73ca936eedd9d15b7048e09275a1ece0
IFTICENE <chermatti.31@gmail.com>
2026-04-09 23:37:14 -0400

    Suppression voiture Mercedes

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from voiture import Voiture
 from crud_db import ajouter_voiture, supprimer_voiture, recuperer_voitures
 
 
-v1 = Voiture("Mercedes", "Classe C", 2024, 55000)
+supprimer_voiture(1)
 
-ajouter_voiture(v1)
-
-print("Mercedes ajoutée avec succès")
+print("Mercedes supprimée avec succès")
